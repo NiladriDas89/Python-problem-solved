ffb01c5479edf323a0470a44aa7d01a62ab91415
REBACK_TECH_E-NIL <128978532+NiladriDas89@users.noreply.github.com>
2024-01-25 20:03:05 +0530

    update

--- Execises/python15.py
+++ Execises/python15.py
@@ -1,4 +1,6 @@
 from math import *
+#Write a Python program to get the volume of a sphere with radius six
+#Formula is 4/3*pi*r^3
 r=6
 v=float(4/3*pi*pow(r,3))
 print(v)
--- Execises/python16.py
+++
@@ -1,4 +0,0 @@
-n=input("Enter the input: ")
-def near_thousand(n):
-  
-    return ((abs(1000 - n) <= 100) or (abs(2000 - n) <= 100))
